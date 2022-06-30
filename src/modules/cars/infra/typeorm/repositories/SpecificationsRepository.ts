4d3a2650f6a836a287c9e8e020a33eaed5bb7850
qwwerty <rhalfsouzaa@gmail.com>
2022-06-29 23:25:04 -0300

    refactor: altera o retorno do método create

--- src/modules/cars/infra/typeorm/repositories/SpecificationsRepository.ts
+++ src/modules/cars/infra/typeorm/repositories/SpecificationsRepository.ts
@@ -14,19 +14,30 @@ class SpecificationsRepository implements ISpecificationsRepostiory {
     this.repository = getRepository(Specification);
   }
 
-  async create({ name, description }: ICreataeSpecificationDTO): Promise<void> {
+  async create({
+    name,
+    description,
+  }: ICreataeSpecificationDTO): Promise<Specification> {
     const specification = this.repository.create({
       description,
       name,
     });
 
     await this.repository.save(specification);
+
+    return specification;
   }
 
   async findByName(name: string): Promise<Specification> {
     const specification = await this.repository.findOne({ name });
 
     return specification;
+  }
+
+  async findByIds(ids: string[]): Promise<Specification[]> {
+    const specifations = await this.repository.findByIds(ids);
+
+    return specifations;
   }
 }
 
