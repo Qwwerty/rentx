c3ffb1274452af19ea848e237f7ec9edf0b38d2d
qwwerty <rhalfsouzaa@gmail.com>
2022-06-29 23:25:54 -0300

    feat: adiciona o método de findsById e o de create

--- src/modules/cars/repositories/ISpecificationsRepository.ts
+++ src/modules/cars/repositories/ISpecificationsRepository.ts
@@ -6,8 +6,12 @@ interface ICreataeSpecificationDTO {
 }
 
 interface ISpecificationsRepostiory {
-  create({ name, description }: ICreataeSpecificationDTO): Promise<void>;
+  create({
+    name,
+    description,
+  }: ICreataeSpecificationDTO): Promise<Specification>;
   findByName(name: string): Promise<Specification>;
+  findByIds(ids: string[]): Promise<Specification[]>;
 }
 
 export { ISpecificationsRepostiory, ICreataeSpecificationDTO };
