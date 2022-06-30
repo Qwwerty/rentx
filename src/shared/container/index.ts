98c998e542f31a36f1c4b07a10cdf0088fd3c7e0
qwwerty <rhalfsouzaa@gmail.com>
2022-06-29 23:29:16 -0300

    feat: adiciona o specificationRepository

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -3,11 +3,11 @@ import { container } from "tsyringe";
 import { UsersRepository } from "@modules/accounts/infra/typeorm/repositories/UsersRepository";
 import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
 import { CategoriesRepository } from "@modules/cars/infra/typeorm/repositories/CategoriesRepository";
-import { SpecificationsRepository } from "@modules/cars/infra/typeorm/repositories/SpecificationsRepository";
 import { ICategoriesRepository } from "@modules/cars/repositories/ICategoriesRepository";
+import { SpecificationsRepository } from "@modules/cars/infra/typeorm/repositories/SpecificationsRepository";
 import { ISpecificationsRepostiory } from "@modules/cars/repositories/ISpecificationsRepository";
-import { ICarsRepository } from "@modules/cars/repositories/ICarsRepository";
 import { CarsRepository } from "@modules/cars/infra/typeorm/repositories/CarsRepository";
+import { ICarsRepository } from "@modules/cars/repositories/ICarsRepository";
 
 container.registerSingleton<ICategoriesRepository>(
   "CategoriesRepository",
